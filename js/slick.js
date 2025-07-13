d4359b9233a65230bec5ce7403d1fe2a42296b28
h_honma <h_honma@forspec.co.jp>
2025-07-13 12:37:14 +0900

    slickの表示形態を変更

--- js/slick.js
+++ js/slick.js
@@ -3,8 +3,12 @@
 // トップページスライドショー
 $('.mainimg-slick').slick({
 	autoplay: true,
-	dots: true,				//丸いページナビボタンを表示
+	dots: false,				//丸いページナビボタンを表示
 	arrows: false,			//左右の矢印
-	autoplaySpeed: 4000,	//切り替えのスピード。デフォルトは3000。
+	autoplaySpeed: 2000,	//切り替えのスピード。デフォルトは3000。
 	pauseOnHover: false,	//マウスオン時にスライドショーをストップするか。
+	slidesToShow: 1,		//画面内に表示させる枚数。
+		slidesToScroll: 1,	
+	centerMode: true,
+    centerPadding: '25%',
 });
