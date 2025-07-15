cb7abacd873733dfb0176bd6e36ec82907b851e4
h_honma <h_honma@forspec.co.jp>
2025-07-15 23:10:46 +0900

    slickのレスポンシブ対応を修正

--- js/slick.js
+++ js/slick.js
@@ -1,14 +1,36 @@
+var windowWidth = $(window).width();
+if (windowWidth < 769) {
+	initializeSlickResponsive();
+} else {
+	initializeSlick();
+}
 
-
-// トップページスライドショー
-$('.mainimg-slick').slick({
-	autoplay: true,
-	dots: false,				//丸いページナビボタンを表示
-	arrows: false,			//左右の矢印
-	autoplaySpeed: 2000,	//切り替えのスピード。デフォルトは3000。
-	pauseOnHover: false,	//マウスオン時にスライドショーをストップするか。
-	slidesToShow: 1,		//画面内に表示させる枚数。
+function initializeSlick() {
+  $('.mainimg-slick').slick({
+		autoplay: true,
+		dots: false,				//丸いページナビボタンを表示
+		arrows: false,			//左右の矢印
+		autoplaySpeed: 2000,	//切り替えのスピード。デフォルトは3000。
+		pauseOnHover: false,	//マウスオン時にスライドショーをストップするか。
+		slidesToShow: 2,		//画面内に表示させる枚数。
 		slidesToScroll: 1,	
-	centerMode: true,
-    centerPadding: '25%',
-});
+		centerMode: true,
+		centerPadding: '25%',
+	});
+	
+}
+
+function initializeSlickResponsive() {
+  $('.mainimg-slick').slick({
+		autoplay: true,
+		dots: false,				//丸いページナビボタンを表示
+		arrows: false,			//左右の矢印
+		autoplaySpeed: 2000,	//切り替えのスピード。デフォルトは3000。
+		pauseOnHover: false,	//マウスオン時にスライドショーをストップするか。
+		slidesToShow: 1,		//画面内に表示させる枚数。
+		slidesToScroll: 1,	
+		centerMode: true,
+		centerPadding: '25%',
+	});
+}
+
